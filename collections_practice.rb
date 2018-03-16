fbbd854b49a42ae3bc598120fdabac444db1a66c
Karen G <kgraves0701@gmail.com>
2018-03-16 23:57:16 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -45,6 +45,6 @@ end
 
 def add_s(array)
   array = array.each_with_index.collect do |element,index|
-    index != 1 ? element = element + "s" : element
+    index != 1 ? element += "s" : element
   end
 end
