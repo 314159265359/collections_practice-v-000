6e7ffc36eb4d28e73b19b2f42d2581bc7ef1a6c4
Karen G <kgraves0701@gmail.com>
2018-03-16 22:50:07 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,3 +6,11 @@ def sort_array_desc(array)
   holder = array.sort
   holder.reverse
 end
+
+def array_char_count(array)
+  counting_array=[]
+  array.each do |element|
+    counting_array << element.count
+  end
+  counting_array.sort
+end
